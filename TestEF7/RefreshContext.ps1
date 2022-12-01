26b5c486783f7817e3c2718305185e32872873e1
josvanvlimmeren <josvanvlimmeren@hotmail.com>
2022-12-01 17:00:01 +0100

    Add files via upload

---
+++ TestEF7/RefreshContext.ps1
@@ -0,0 +1,3 @@
+﻿$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+CD $PSScriptRoot
+dotnet ef dbcontext scaffold "Server=ServerName;Database=DEV_TestTemp;Uid=tmp_user;Pwd='NotHardPassword1!';MultipleActiveResultSets=true;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer --output-dir Models --context "TempDataContext" --data-annotations --force --no-onconfiguring
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestEF7.Models;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TempDataContext Context;
+
+        public HomeController(ILogger<HomeController> logger, TempDataContext context)
+        {
+            _logger = logger;
+            Context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var firstUser = Context.AspNetUsers.FirstOrDefault();
+            var userRoles = firstUser.Roles.ToList(); //This gives an error!
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
